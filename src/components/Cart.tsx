e4074c81a3d19f35148ffaac5cd7b5839dda6060
Oleh Zinchenko <olegzinchenko1983@gmail.com>
2026-02-09 21:10:10 +0000

    minor: deleted redundend code

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,21 +1,18 @@
 import { createPortal } from 'react-dom'
 
 import CartItems from './CartItems.tsx'
-import { useAppSelector } from '../store/hooks.ts'
 
 type CartProps = {
   onClose: () => void
 }
 
 export default function Cart({ onClose }: CartProps) {
-  const cartItems = useAppSelector((state) => state.cart.items)
-
   return createPortal(
     <>
       <div className='cart-backdrop' />
       <dialog id='cart-modal' open>
         <h2>Your Cart</h2>
-        <CartItems cartItems={cartItems} />
+        <CartItems />
         <p id='cart-actions'>
           <button onClick={onClose}>Close</button>
         </p>
